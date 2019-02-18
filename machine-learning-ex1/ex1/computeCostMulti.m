da61af3d9ae6ad1b74a50428c89632a1505770e3
yangmen0729 <kaldimen@hotmail.com>
2019-02-17 20:45:19 -0500

    Add files via upload

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,8 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+prediction = X * theta;
+SSE = (prediction - y) .^ 2;
+J = (1 / (2 * m)) * sum(SSE);
 
 
 % =========================================================================
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -17,10 +17,9 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
-
-
+    h = X * theta;
+    % X' * (h - y) = sum((h - y) .* X)'
+    theta -= alpha * (1 / m) * (X' * (h - y));
 
 
 
